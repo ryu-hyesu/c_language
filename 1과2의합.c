11ba3f90c2f7ff224aa7d4e9925e5e16befed571
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2022-06-04 18:37:24 +0900

    Add files via upload

---
+++ 1과2의합.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int pick(int itemSize, int* bucket, int bucketSize, int k,int n){
+	int smallest, lastIndex, item;
+	int i, sum = 0, count = 0;
+
+	for (i = 0; i < bucketSize - k; i++)
+		sum += bucket[i];
+
+	if (sum == n) return 1;
+	if (sum > n) return 0;
+	if (k == 0) return 0;
+
+	lastIndex = bucketSize - k - 1;
+	if (bucketSize == k)
+		smallest = 1;
+	else
+		smallest = bucket[lastIndex];
+
+	for (item = smallest; item <= itemSize; item++) {
+		bucket[lastIndex + 1] = item;
+		count += pick(itemSize, bucket, bucketSize,k - 1,n);
+	}
+	return count;
+
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int* picked = (int*)malloc(sizeof(int) * n);
+
+	printf("%d", pick(2, picked, n, n, n));
+}
---
+++ 피보나치.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int fibonaci(int n, int* M) {
+	if (n == 0 || n == 1) 
+		M[n]++;
+	else 
+		fibonaci(n - 1, M) + fibonaci(n - 2, M);
+	
+}
+int main()
+{	
+	int n;
+	scanf("%d", &n);
+
+	int M[2] = { 0,0 };
+
+	fibonaci(n, M);
+	printf("%d %d", M[1], M[0]);
+}
